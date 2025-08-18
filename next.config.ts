f0fbffe4701c18bcea8663476edc335ffc184cb7
mvip07 <mirabzalozodov07@gmail.com>
2025-08-18 13:26:11 +0500

    update 9

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+const path = require("path");
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -19,6 +20,21 @@ const nextConfig: NextConfig = {
                 pathname: '/**',
             },
         ],
+    },
+    webpack: (config) => {
+        // Ichki yo‘llarni qo‘llab-quvvatlash uchun aliaslarni tuzatish (/* qo‘shildi)
+        config.resolve.alias["@components/*"] = path.resolve(__dirname, "src/app/[lang]/components/*");
+        config.resolve.alias["@hooks/*"] = path.resolve(__dirname, "src/hooks/*");
+        config.resolve.alias["@features/*"] = path.resolve(__dirname, "src/features/*");
+        config.resolve.alias["@services/*"] = path.resolve(__dirname, "src/services/*");
+        config.resolve.alias["@styles/*"] = path.resolve(__dirname, "src/styles/*");
+        config.resolve.alias["@types/*"] = path.resolve(__dirname, "src/types/*");
+        config.resolve.alias["@lib/*"] = path.resolve(__dirname, "src/lib/*");
+        config.resolve.alias["@context/*"] = path.resolve(__dirname, "src/context/*");
+        config.resolve.alias["@assets/*"] = path.resolve(__dirname, "src/assets/*");
+        config.resolve.alias["@stores/*"] = path.resolve(__dirname, "src/stores/*");
+
+        return config;
     },
 };
 
